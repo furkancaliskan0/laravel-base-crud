f0e0751e2e492b6d250e165a64fbac021679b7cf
furkanGit <furkancaliskan9801@gmail.com>
2023-02-02 17:57:15 +0100

    created saints factories

--- database/factories/SaintsFactory.php
+++ database/factories/SaintsFactory.php
@@ -5,7 +5,7 @@ namespace Database\Factories;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Model>
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Saints>
  */
 class SaintsFactory extends Factory
 {
@@ -17,7 +17,10 @@ class SaintsFactory extends Factory
     public function definition()
     {
         return [
-            //
+            'nome' => fake()->firstName(),
+            'luogo_di_nascita' => fake()->city(),
+            'data_benedizione' => fake()->dateTimeAD(),
+            'numero_miracoli' => fake()->randomNumber(5, true)
         ];
     }
 }
